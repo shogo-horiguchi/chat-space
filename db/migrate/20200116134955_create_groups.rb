ad3a851875d41680147b44f5931134bdb0accd49
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-01-16 22:55:00 +0900

    edit 2 migration files
    group model and member model

--- db/migrate/20200116134955_create_groups.rb
+++ db/migrate/20200116134955_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false
+      t.index :name, unique: true
       t.timestamps
     end
   end

48f5f2c0efd7e39d77a6b601e309ddba82e4f2ec
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-01-16 23:41:32 +0900

    complete new and create action on groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,24 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
   end
   
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
+
 end

3b19ccc8ef9c8d671f6844093b89b4ef913b916a
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-01-18 12:07:52 +0900

    add routing about new message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,9 @@
 class MessagesController < ApplicationController
+  
   def index
   end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :message, only: [:create]
+  end
 end

ff8f93def0dd05e1f9a3f1256bd54520b8d6c892
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-01-19 13:34:33 +0900

    show latest message on sideBar

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :members
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body?
+        last_message.body
+      else
+        '画像が投稿されています。'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end

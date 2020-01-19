55a16e82adedf21595e4df55a344df2e5d113703
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-01-19 13:20:24 +0900

    show group messages on minChat

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [200, 200]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir

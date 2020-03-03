7de59cae27aa0d8b367151b93aa5b1165ae22725
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-03-03 22:41:32 +0900

    prepare 'create.json.jbuilder'

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,18 @@
 $(function(){
-
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      console.log(message);
+    })
+  });
 });

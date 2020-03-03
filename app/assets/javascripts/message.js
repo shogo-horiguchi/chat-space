5534bbdb04ee726eded23a40ba8ce4390672a931
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-03-03 23:38:44 +0900

    prepare animate action

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+
+  function buildHTML(message){
+    if( message.image){
+      var html = `<div class="mainChat__messageList--detail">
+                      <ul>
+                        <li class="mainChat__messageList--contributor">
+                        ${message.user_name}
+                        </li>
+                        <li class="mainChat__messageList--date">
+                        ${message.created_at}
+                        </li>
+                      </ul>
+                      <div class="mainChat__messageList--content">
+                        <p>
+                        ${message.body}
+                        </p>
+                      <img src=${message.image}>
+                      </div>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="mainChat__messageList--detail">
+                      <ul>
+                        <li class="mainChat__messageList--contributor">
+                        ${message.user_name}
+                        </li>
+                        <li class="mainChat__messageList--date">
+                        ${message.created_at}
+                        </li>
+                      </ul>
+                      <div class="mainChat__messageList--content">
+                        <p>
+                        ${message.body}
+                        </p>
+                      </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -12,7 +52,10 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      console.log(message);
+      var html = buildHTML(message);
+      $('.mainChat__messageList--details').append(html);
+      $('.mainChat__messageList').animate({ scrollTop: $('.mainChat__messageList')[0].scrollHeight});
+      $('#message_body').val('');
     })
   });
 });

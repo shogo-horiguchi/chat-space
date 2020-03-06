e9c345c3a5e2c917946a63ca4632476b38a9c7d1
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-03-06 21:14:04 +0900

    draw html view when ajax happen

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,23 @@
 $(function() {
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -8,10 +27,20 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users) {
-      console.log("成功です");
+      $("#user-search-result").empty();
+
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function() {
-      console.log("失敗です");
+      alert("通信エラーです。ユーザーが表示できません。");
     });
   });
 });

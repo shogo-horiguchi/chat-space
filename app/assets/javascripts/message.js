aa23fe89dc6de0e3eec7b7e0af75d4d191fd90d5
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-03-07 15:39:38 +0900

    complete implementation befor review

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,36 +64,33 @@ $(function(){
       $('.mainChat__messageForm--submitBtn').prop("disabled", false);
     });
   });
-});
 
-
-$(function(){
-
-  var reloadMessages = function(){
-    var last_message_id = $('.mainChat__messageList--detail:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: "GET",
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages){
-      if (message.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message){
-          insertHTML += buildHTML(message)
-        });
-        $('.mainChat__messageList--detail').append(insertHTML);
-        $('.mainChat__messageList').animate({ scrollTop: $('.mainChat__messageList')[0].scrollHeight});
-      }
-    })
-    .fail(function(){
-      alert('error');
-    })
-  };
+    var reloadMessages = function(){
+      var last_message_id = $('.mainChat__messageList--detail:last').data("message-id");
+      console.log(last_message_id)
+      $.ajax({
+        url: "api/messages",
+        type: "GET",
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        console.log(messages);
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message){
+            insertHTML += buildHTML(message)
+          });
+          $('.mainChat__messageList--details').append(insertHTML);
+          $('.mainChat__messageList').animate({ scrollTop: $('.mainChat__messageList')[0].scrollHeight});
+        }
+      })
+      .fail(function(){
+        alert('error');
+      })
+    };
 
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
-
 });
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,6 +2,6 @@ class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
-    @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+    @messages = group.messages.includes(:user).where('id > ?', last_message_id)
   end
 end

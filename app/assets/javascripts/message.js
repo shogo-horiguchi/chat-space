84a93983b6176c1a33baee91c21ea8eedd25b322
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-03-07 15:52:50 +0900

    delete codes(for debug)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -67,7 +67,6 @@ $(function(){
 
     var reloadMessages = function(){
       var last_message_id = $('.mainChat__messageList--detail:last').data("message-id");
-      console.log(last_message_id)
       $.ajax({
         url: "api/messages",
         type: "GET",
@@ -75,7 +74,6 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages){
-        console.log(messages);
         if (messages.length !== 0) {
           var insertHTML = '';
           $.each(messages, function(i, message){
